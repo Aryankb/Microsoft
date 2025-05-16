ff0364cb9e7e03b395409be467a33d92cf364ad6
Aryankb <aryan22102@iiitnr.edu.in>
2025-05-16 23:16:21 +0530

    again added token

--- src/components/FetchLogs.tsx
+++ src/components/FetchLogs.tsx
@@ -24,8 +24,7 @@ export const useWorkflowLogs = (onLogReceived: (log: LogMessage) => void) => {
 
         try {
             const token = await getToken();
-            // const ws = new WebSocket(`wss://backend.sigmoyd.in/ws?token=${token}`);
-            const ws = new WebSocket(`wss://backend.sigmoyd.in/ws`);
+            const ws = new WebSocket(`wss://backend.sigmoyd.in/ws?token=${token}`);
             wsRef.current = ws;
 
             ws.onopen = () => {
---
+++ try.js
@@ -0,0 +1,1 @@
+const socket = new WebSocket("ws://backend.sigmoyd.in:8000/ws");
